dd0a7b16049daaece7b4374c6952d598067137fa
rangreza <81700083+rangreza@users.noreply.github.com>
2024-09-14 16:51:53 +0530

    Update awseks_main.tf
    17

--- awseks_main.tf
+++ awseks_main.tf
@@ -2,60 +2,59 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-# VPC Module (creates VPC, subnets, internet gateway, etc.)
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "4.0.0"
+  version = "3.14.2"
 
-  name = "eks-vpc"
+  name = "my-vpc"
   cidr = "10.0.0.0/16"
 
   azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  private_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
-  single_nat_gateway = true
-
-  tags = {
-    Name = "eks-vpc"
-  }
 }
 
-# EKS Cluster (without node_groups argument)
 module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  version         = "18.29.0"
-
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.26"
-  vpc_id          = module.vpc.vpc_id
-  subnet_ids      = module.vpc.private_subnets
-
-  tags = {
-    Environment = "dev"
-    Name        = "eks-cluster"
-  }
-}
-
-# Managed Node Group Configuration
-module "eks_managed_node_group" {
-  source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+  source  = "terraform-aws-modules/eks/aws"
   version = "18.29.0"
 
-  cluster_name = module.eks.cluster_name
-  subnet_ids   = module.vpc.private_subnets
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.24"
+  subnets         = module.vpc.private_subnets
+  vpc_id          = module.vpc.vpc_id
 
   node_groups = {
-    eks_node_group = {
+    eks_nodes = {
       desired_capacity = 2
       max_capacity     = 3
       min_capacity     = 1
-      instance_type    = "t3.medium"
+
+      instance_type = "t3.medium"
     }
   }
+}
 
-  tags = {
-    Name = "eks-node-group"
-  }
+module "eks_managed_node_group" {
+  source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+  
+  cluster_name = module.eks.cluster_id
+  node_group_name = "my-eks-node-group"
+  subnets = module.vpc.private_subnets
+  instance_type = "t3.medium"
+  
+  desired_size = 2
+  max_size     = 3
+  min_size     = 1
+}
+
+output "cluster_endpoint" {
+  description = "EKS cluster endpoint"
+  value       = module.eks.cluster_endpoint
+}
+
+output "cluster_security_group_id" {
+  description = "EKS cluster security group ID"
+  value       = module.eks.cluster_security_group_id
 }
