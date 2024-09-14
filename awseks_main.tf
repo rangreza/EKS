4fc17a3a12a82f92b7a7cfeb51a53bed62aa624c
rangreza <81700083+rangreza@users.noreply.github.com>
2024-09-14 17:31:33 +0530

    Update awseks_main.tf
    34

--- awseks_main.tf
+++ awseks_main.tf
@@ -39,21 +39,17 @@ module "eks" {
 
 module "eks_managed_node_group" {
   source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
-  version = "18.29.0"
+  version = "20.8.4"
 
   cluster_name = module.eks.cluster_name
+  node_group_name = "my-managed-node-group"
+  
+  # Define the node group details
+  desired_capacity = 2
+  max_capacity     = 3
+  min_capacity     = 1
+  instance_type    = "t3.medium"
 
-  # Define the node group
-  node_groups = {
-    default = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_type    = "t3.medium"
-    }
-  }
-
-  vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
 
   tags = {
