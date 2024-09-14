95d43c97c5c0695f3b83a4881a6d19ce26d172f9
rangreza <81700083+rangreza@users.noreply.github.com>
2024-09-14 16:56:41 +0530

    Update awseks_main.tf
    19

--- awseks_main.tf
+++ awseks_main.tf
@@ -1,35 +1,53 @@
-module "eks" {
-  source          = "terraform-aws-modules/eks/aws"
-  cluster_name    = var.cluster_name
-  cluster_version = var.kubernetes_version
-  vpc_id          = module.vpc.vpc_id
-
-  # Specify only 1 public and 1 private subnet
-  vpc_subnets     = [module.vpc.public_subnets[0], module.vpc.private_subnets[0]]
-
-  # Enable cluster logging
-  enable_irsa     = true
-
-  # Managed Node Groups
-  node_groups = {
-    eks_nodes = {
-      desired_capacity = 2
-      max_capacity     = 3
-      min_capacity     = 1
-      instance_type    = "t3.medium"
-    }
-  }
+provider "aws" {
+  region = "ap-south-1"
 }
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  name    = "eks-vpc"
-  cidr    = "10.0.0.0/16"
+  version = "3.19.0"
+
+  name = "my-vpc"
+  cidr = "10.0.0.0/16"
 
   azs             = ["ap-south-1a", "ap-south-1b"]
   public_subnets  = ["10.0.1.0/24"]
   private_subnets = ["10.0.2.0/24"]
 
-  enable_nat_gateway = true
-  single_nat_gateway = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "my-vpc"
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "18.29.0"
+
+  cluster_name    = "my-eks-cluster"
+  cluster_version = "1.27"
+  subnets         = concat(module.vpc.public_subnets, module.vpc.private_subnets)
+  vpc_id          = module.vpc.vpc_id
+
+  enable_irsa = true
+  manage_aws_auth = true
+}
+
+module "eks_managed_node_group" {
+  source  = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
+  version = "18.29.0"
+
+  cluster_name = module.eks.cluster_id
+  node_group_name = "my-managed-node-group"
+
+  node_group_defaults = {
+    instance_type = "t3.medium"
+    desired_capacity = 2
+    max_capacity     = 3
+    min_capacity     = 1
+  }
+
+  vpc_id  = module.vpc.vpc_id
+  subnets = module.vpc.private_subnets
 }
