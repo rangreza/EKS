37667652625566453d292fbd20618bae98adf700
rangreza <81700083+rangreza@users.noreply.github.com>
2024-09-14 15:12:51 +0530

    Update s3backend.tf
    ap-south-1 Mumbai

--- s3backend.tf
+++ s3backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "tfstate"
     key            = "path/to/your/statefile.tfstate"
-    region         = "us-west-2"
+    region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "dynamolck"
   }
