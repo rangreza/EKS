6ab1ba6b840bdc0450637503572851604597af6f
rangreza <81700083+rangreza@users.noreply.github.com>
2024-09-14 17:53:49 +0530

    Update awseks_main.tf
    42

--- awseks_main.tf
+++ awseks_main.tf
@@ -37,12 +37,40 @@ module "eks" {
   }
 }
 
+resource "aws_iam_role" "node_role" {
+  name = "eks-node-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole",
+        Effect    = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "node_role_attachment" {
+  role       = aws_iam_role.node_role.name
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
+resource "aws_iam_role_policy_attachment" "node_role_attachment_2" {
+  role       = aws_iam_role.node_role.name
+  policy_arn  = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
 resource "aws_eks_node_group" "default" {
   cluster_name    = module.eks.cluster_name
   node_group_name = "default-node-group"
-  node_role_arn   = module.eks.node_role_arn
-  subnets         = module.vpc.private_subnets
+  node_role_arn   = aws_iam_role.node_role.arn
+  subnet_ids      = module.vpc.private_subnets
   instance_types  = ["t3.medium"]
+
   scaling_config {
     desired_size = 2
     max_size     = 3
