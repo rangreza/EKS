ce1b3472a2ca43b7d945f8a61c1905241ebea7c5
rangreza <81700083+rangreza@users.noreply.github.com>
2024-09-14 16:54:48 +0530

    Update awseks_main.tf
    18

--- awseks_main.tf
+++ awseks_main.tf
@@ -1,60 +1,35 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "3.14.2"
-
-  name = "my-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["ap-south-1a", "ap-south-1b", "ap-south-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
-
-  enable_nat_gateway = true
-}
-
 module "eks" {
-  source  = "terraform-aws-modules/eks/aws"
-  version = "18.29.0"
-
-  cluster_name    = "my-eks-cluster"
-  cluster_version = "1.24"
-  subnets         = module.vpc.private_subnets
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = var.kubernetes_version
   vpc_id          = module.vpc.vpc_id
 
+  # Specify only 1 public and 1 private subnet
+  vpc_subnets     = [module.vpc.public_subnets[0], module.vpc.private_subnets[0]]
+
+  # Enable cluster logging
+  enable_irsa     = true
+
+  # Managed Node Groups
   node_groups = {
     eks_nodes = {
       desired_capacity = 2
       max_capacity     = 3
       min_capacity     = 1
-
-      instance_type = "t3.medium"
+      instance_type    = "t3.medium"
     }
   }
 }
 
-module "eks_managed_node_group" {
-  source = "terraform-aws-modules/eks/aws//modules/eks-managed-node-group"
-  
-  cluster_name = module.eks.cluster_id
-  node_group_name = "my-eks-node-group"
-  subnets = module.vpc.private_subnets
-  instance_type = "t3.medium"
-  
-  desired_size = 2
-  max_size     = 3
-  min_size     = 1
-}
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  name    = "eks-vpc"
+  cidr    = "10.0.0.0/16"
 
-output "cluster_endpoint" {
-  description = "EKS cluster endpoint"
-  value       = module.eks.cluster_endpoint
-}
+  azs             = ["ap-south-1a", "ap-south-1b"]
+  public_subnets  = ["10.0.1.0/24"]
+  private_subnets = ["10.0.2.0/24"]
 
-output "cluster_security_group_id" {
-  description = "EKS cluster security group ID"
-  value       = module.eks.cluster_security_group_id
+  enable_nat_gateway = true
+  single_nat_gateway = true
 }
